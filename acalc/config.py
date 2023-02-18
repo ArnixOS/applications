760083b3031ca229c5368bc2ecd1180230fa174b
Abhiraj Rik <97382691+sontaimnt@users.noreply.github.com>
2023-02-18 23:00:12 +0530

    Create config.py

---
+++ acalc/config.py
@@ -0,0 +1,14 @@
+### ---- acalc config file --- ###
+
+bg = "#24283b"
+fg = "#c0caf5"
+
+### ---Colors .. opearators like + are yellow , numbers are blue , functions like sin are green and error is red
+
+operators = "#e0af68"
+numbers = "#7aa2f7"
+functions = "#9ece6a"
+error = "#f7768e"
+
+### --- Default Font --- ###
+font = "CaskaydiaCove Nerd Font"
